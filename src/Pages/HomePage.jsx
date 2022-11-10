022aaa5b332444076491c6f85f27b33742588405
Rinki8085 <krinki2604@gmail.com>
2022-11-10 11:27:58 +0530

    caliper_assignment

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Component/Navbar';
+import Footer from '../Component/Footer';
+
+function Homepage(){
+   return(
+      <div>
+         <Navbar/>
+         {/* <Footer/> */}
+      </div>
+   )
+}
+
+export default Homepage;
